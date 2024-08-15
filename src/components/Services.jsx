a0d7194e17c27c5bbdbc45c723c25727c790d7a2
Mohammed Zain ul Hassan <powehi29@gmail.com>
2024-08-16 00:01:09 +0500

    Modified Heading to accept text

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,9 +1,15 @@
 import Section from "./Section";
+import Heading from "./Heading";
 
 const Services = () => {
   return (
     <Section id="how-to-use">
-      <div className="container"></div>
+      <div className="container">
+        <Heading
+          title="Generative AI made for creators."
+          text="Brainwave unlocks the potential of AI-powered applications"
+        />
+      </div>
     </Section>
   );
 };
