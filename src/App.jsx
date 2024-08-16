c8ceae5c0302f2abf8e5760dc6c693ee724030ff
Mohammed Zain ul Hassan <powehi29@gmail.com>
2024-08-16 23:53:16 +0500

    Added Roadmap Section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Benefits from "./components/Benefits";
 import Collaboration from "./components/Collaboration";
 import Services from "./components/Services";
 import Pricing from "./components/Pricing";
+import Roadmap from "./components/Roadmap";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Collaboration />
         <Services />
         <Pricing />
+        <Roadmap />
       </div>
       <ButtonGradient />
     </>
